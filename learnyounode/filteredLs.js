772520bbb63ce9fefcae208f64dc85b117ae5c82
Fernando <ffmp777@yahoo.com>
2017-04-11 05:34:50 -0600

    completed filtered ls

---
+++ learnyounode/filteredLs.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+    path = require('path');
+
+var dir = process.argv[2];
+    ext = '.' + process.argv[3];
+
+fs.readdir(dir, function(err, files) {
+    files.forEach(function(list) {
+        if (path.extname(list) === ext) {
+            console.log(list);
+        }
+    });
+});
+
