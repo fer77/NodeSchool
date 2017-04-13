ecf6e3b857db3017cfd46fe30daf1355c6856197
Fernando <ffmp777@yahoo.com>
2017-04-13 17:10:24 -0600

    set up for next exercise

---
+++ learnyounode/makeModular.js
@@ -0,0 +1,18 @@
+var dir = process.argv[2];
+    ext = process.argv[3];
+
+var myModule = require('./modules');
+
+function callback(err, list) {
+    if (err) {
+
+        return console.log('Error:', err);
+    }
+    list.forEach(function(item) {
+
+        console.log(item);
+    });
+};
+
+myModule(dir, ext, callback);
+
