0b81150444242fa41cfab9db1cbbc95e7854f979
Fernando <ffmp777@yahoo.com>
2017-04-18 16:46:38 -0600

    completed time server

---
+++ learnyounode/timeServer.js
@@ -0,0 +1,26 @@
+var net = require('net'),
+    portNumber = process.argv[2] || 8000,
+    date = new Date(),
+    year = date.getFullYear(),
+    month = date.getMonth() + 1,     // starts at 0
+    day = date.getDate(),      // returns the day of month
+    hour = date.getHours(),
+    minutes = date.getMinutes();
+
+if (month < 10) {
+    month = '0' + month;
+};
+
+if (day < 10) {
+    day = '0' + day;
+};
+
+date = year + "-" + month + "-" + day + " " + hour + ":" + minutes; //"YYYY-MM-DD hh:mm"
+
+     var server = net.createServer(function(socket) {
+       // socket handling logic
+         socket.end(date + "\n");
+     });
+
+server.listen(portNumber);
+
