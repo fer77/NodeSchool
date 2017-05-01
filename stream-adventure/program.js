3f9a6f2e230385defba3cbc077bf1c86b407c51e
Fernando <ffmp777@yahoo.com>
2017-05-01 17:09:52 -0600

    completed concat

--- stream-adventure/program.js
+++
@@ -1,8 +0,0 @@
-var through = require('through2');
-var split = require('split');
-    process.stdin
-        .pipe(split())
-        .pipe(through2(function (buffer, encoding, next) {
-            console.dir(line.toString());
-            next();
-        }));
