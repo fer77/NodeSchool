aaf5d01d1fc8c0fb49759c2f3e8d40ca22292111
Fernando <ffmp777@yahoo.com>
2017-05-16 06:41:40 -0600

    trampoline

--- functional-javascript-workshop/trampoline.js
+++ functional-javascript-workshop/trampoline.js
@@ -1,25 +1,22 @@
-
 function repeat(operation, num) {
   // Modify this so it doesn't cause a stack overflow!
-  if (num <= 0) return
-  
-  return function() {
-    operation()
-    return repeat(operation, --num)
-  } 
-}
+  if (num <= 0) {
+    return function() {
+      operation();
+      return repeat(operation, --num);
+    } 
+  };
+};
 
 function trampoline(fn) {
-  while (fn && typeof fn === 'function') {
+  while (fn && fn instanceof Function) {
     fn = fn();
-  }
-
-  return fn;
-}
+  };
+};
 
 module.exports = function(operation, num) {
   // You probably want to call your trampoline here!
   return trampoline(function() {
-    return repeat(operation, num)
-  })
-}
+    return repeat(operation, num);
+  });
+};
