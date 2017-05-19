72e64200e628398eb9580dab029fec2bafb60bfd
Fernando <ffmp777@yahoo.com>
2017-05-19 13:07:49 -0600

    currying

---
+++ functional-javascript-workshop/currying.js
@@ -0,0 +1,13 @@
+function curryN(fn, n) {
+  n = n || fn.length;
+
+  return function(arg) {
+    console.log(arg);
+    if(n <= 1) {
+      return fn(arg);
+    } 
+    return curryN(fn.bind(this, arg), n - 1);
+  }
+}
+
+    module.exports = curryN;
