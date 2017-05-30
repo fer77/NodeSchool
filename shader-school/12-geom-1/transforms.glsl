f275ae8c162cf90e1849edda2bdc3ef675633545
Fernando <ffmp777@yahoo.com>
2017-05-30 07:31:28 -0600

    clip coordinates

--- shader-school/12-geom-1/transforms.glsl
+++ shader-school/12-geom-1/transforms.glsl
@@ -4,9 +4,13 @@ attribute vec3 position;
 
 uniform mat4 model, view, projection;
 
+// model: transforms the object from data coordinates to world coordinates. Controls the location of the object in the world.
+// view: transforms world coordinate system to a viewing coordinate system. Controls the position and orientation of the camera.
+// projection: transforms the view coordinate system into device clip coordinates. Controls whether the view is orthographic or perspective, and also controls the aspect ratio of the camera.
+
 void main() {
 
   //TODO: Apply the model-view-projection matrix to `position`
+  gl_Position = (projection * view * model) * vec4(position, 1.0);
 
-  gl_Position = vec4(position, 1);
 }
