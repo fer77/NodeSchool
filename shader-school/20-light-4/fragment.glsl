bbe50311a687c7436186e9ab6aa4476d9c3dd809
Fernando <ffmp777@yahoo.com>
2017-06-01 17:01:49 -0600

    point light

--- shader-school/20-light-4/fragment.glsl
+++ shader-school/20-light-4/fragment.glsl
@@ -1,21 +1,24 @@
 precision mediump float;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
-uniform mat4 inverseModel;
-uniform mat4 inverseView;
-uniform mat4 inverseProjection;
-
 uniform vec3 ambient;
 uniform vec3 diffuse;
 uniform vec3 specular;
-
 uniform vec3 lightPosition;
 
 uniform float shininess;
 
+varying vec3 fragNormal;
+varying vec3 fragPosition;
+varying vec3 lightDirection;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec3 eyeDirection = normalize(fragPosition);
+  vec3 normal = normalize(fragNormal);
+  vec3 light = normalize(lightDirection);
+  
+  float lambert = max(dot(normal, light), 0.0);
+  float phong = pow(max(dot(reflect(light, normal), eyeDirection), 0.0), shininess);
+
+  vec3 lightColor = ambient + diffuse * lambert + specular * phong;
+  gl_FragColor = vec4(lightColor, 1);
 }
