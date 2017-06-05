043b977c91b7340f4ceba7a48f5f7c091bf71c8f
Fernando <ffmp777@yahoo.com>
2017-06-05 07:35:59 -0600

    point sprites

--- shader-school/27-prims-1/fragment.glsl
+++ shader-school/27-prims-1/fragment.glsl
@@ -1,5 +1,11 @@
 precision highp float;
 
+varying vec3 fragColor;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+	//gives the coordinate of the fragment in the sprite starting from the upper left corner.
+  if(distance(gl_PointCoord.st, vec2(0.5,0.5)) > 0.5) {
+    discard;
+  }
+  gl_FragColor = vec4(fragColor,1.0);
 }
--- shader-school/27-prims-1/vertex.glsl
+++ shader-school/27-prims-1/vertex.glsl
@@ -1,10 +1,15 @@
 precision highp float;
 
-attribute vec3 position, color;
-attribute float size;
+attribute vec3 position; //the position of each point sprite
+attribute vec3 color; //the color of each point sprite
+attribute float size; //the desired radius of the point sprite in pixels
 
 uniform mat4 model, view, projection;
 
+varying vec3 fragColor;
+
 void main() {
   gl_Position = projection * view * model * vec4(position, 1.0);
+  gl_PointSize = size; //controls the radius of the point in pixels, and can be assigned by the vertex shader.
+  fragColor = color;
 }
