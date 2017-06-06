39bb120546e88a225849bb793ded08a498755033
Fernando <ffmp777@yahoo.com>
2017-06-06 06:52:36 -0600

    triangles

--- shader-school/28-prims-2/fragment.glsl
+++ shader-school/28-prims-2/fragment.glsl
@@ -3,5 +3,10 @@ precision highp float;
 uniform vec3 frontColor, backColor;
 
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+	//Triangle primitives in GLSL get a special property called called gl_FrontFacing which tests if they are oriented towards the camera or not.
+  if(gl_FrontFacing) {
+    gl_FragColor = vec4(frontColor, 1.0); //facing towards the camera
+  } else {
+    gl_FragColor = vec4(backColor, 1.0); //facing away from the camera
+  }
 }
