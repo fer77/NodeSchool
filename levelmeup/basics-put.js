e292fb1e8d9932b83872473c77e69eb74ed57d72
Fernando <ffmp777@yahoo.com>
2017-06-09 06:53:24 -0600

    cleaned up solution for basic put

--- levelmeup/basics-put.js
+++ levelmeup/basics-put.js
@@ -1,16 +1,16 @@
-var level = require('level')
+var level = require('level');
 
 module.exports = function (databaseDir, obj, callback) {
-  var db = level(databaseDir)
-  var error
+  var db = level(databaseDir);
+  var error;
 
   for (var key in obj) {
-    db.put(key, obj[key])
+    db.put(key, obj[key]);
   }
   db.on('error', function (err) {
-    error = err
-  })
+    error = err;
+  });
   db.close(function (err) {
-    callback(error || err)
-  })
+    callback(error || err);
+  });
 }
