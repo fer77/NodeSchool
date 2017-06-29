f9afc097f9f764c11332c5068c6b381f4e6c398f
Fernando <ffmp777@yahoo.com>
2017-06-29 07:08:56 -0600

    clean up lineSplitter.js

--- bytewiser/lineSplitter.js
+++ bytewiser/lineSplitter.js
@@ -1,15 +1,3 @@
-// var through = require('through2'),
-// 	split = require('split'),
-// 	lineCount = 0,
-// 	write = through(function (buffer, encoding, next) {
-// 		var line = buffer.toString();
-// 		this.push(lineCount % 2 === 0 ? line.toLowerCase() + '\n' : line.toUpperCase() + '\n');
-// 		lineCount ++;
-// 		next();
-// 	});
-//
-// 	process.stdin.pipe(split()).pipe(write).pipe(process.stdout);
-
 var fs = require('fs'),
     file = fs.readFileSync(process.argv[2]),
     offset = 0,
